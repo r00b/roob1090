07c804680a0650b1b513b2a31b990eeae1707a75
Robert Steilberg <rsteilberg@gmail.com>
2020-05-15 13:36:27 -0400

    endpoints for filtering flights

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const flightData = require('./../services/flight-data');
+const store = require('./../services/flight-store');
 const _ = require('lodash');
 
-router.get('/', function (req, res, next) {
-  console.log('GET');
-  res.json(flightData.getRawData());
-});
-
-router.ws('/pump', function (ws, req) {
-  ws.on('message', function (data) {
-    try {
-      const json = JSON.parse(data);
-      const result = flightData.setNewData(json);
-      console.log(`success: ${result}`);
-    } catch (e) {
-      console.log('fail');
-    }
+router
+  .get('/raw', function (req, res, next) {
+    res.json(store.getRawData());
+  })
+  .get('/real', function (req, res, next) {
+    res.json(store.getRealAircraft());
+  })
+  .get('/fake', function (req, res, next) {
+    res.json(store.getFakeAircraft());
+  })
+  .get('/taxi', function (req, res, next) {
+    res.json(store.getTaxiingAircraft());
+  })
+  .ws('/pump', function (ws, req) {
+    ws.on('message', function (data) {
+      try {
+        const json = JSON.parse(data);
+        const result = store.setNewData(json);
+        console.log(`success: ${result}`);
+      } catch (e) {
+        console.log('fail');
+      }
+    });
   });
-});
 
 module.exports = router;
---
+++ services/flight-store.js
@@ -0,0 +1,69 @@
+const _ = require('lodash');
+const FLIGHT_TIMEOUT = 20;
+
+module.exports = {
+  setNewData,
+  getRawData,
+  getRealAircraft,
+  getFakeAircraft,
+  getTaxiingAircraft,
+};
+
+// CREATE
+
+/**
+ * Accept new data from source; ensure it is not stale
+ */
+function setNewData (data) {
+  const currTimestamp = _.get(this.getRawData(), 'now');
+  const newTimestamp = data.now;
+  if (newTimestamp > currTimestamp) {
+    this.rawData = data;
+    const partition = this._filterAndSetAircraft(data);
+    this.includedAircraft = partition.included;
+    this.excludedAircraft = partition.excluded;
+    return true;
+  }
+  return false;
+}
+
+function _filterAndSetAircraft (data) {
+  const aircraft = data.aircraft || [];
+  let includedAircraft = [];
+  let excludedAircraft = [];
+  if (aircraft.length) {
+    includedAircraft = _.filter(aircraft, _isValidFlight);
+    excludedAircraft = _.reject(aircraft, _isValidFlight)
+  }
+  return {
+    included: includedAircraft,
+    excluded: excludedAircraft
+  }
+}
+
+function _isValidFlight (flight) {
+  const hasPosition = flight.lat && flight.long;
+  const hasIdent = flight.flight;
+  const isRecent = flight.seen < FLIGHT_TIMEOUT;
+  return hasPosition && hasIdent && isRecent;
+}
+
+// RETRIEVE
+
+/**
+ * Return the most up-to-date, raw JSON that is currently in the store
+ */
+function getRawData () {
+  return this.rawData || {
+    now: -1,
+    aircraft: []
+  };
+}
+
+function getRealAircraft () {
+  return this.includedAircraft || [];
+}
+
+function getFakeAircraft () {
+  return this.excludedAircraft || [];
+}

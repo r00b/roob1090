ed032e4bc72c8d99c87c958c1bacbc9413ebf568
Robert Steilberg <rsteilberg@gmail.com>
2020-07-15 08:17:03 -0400

    add dump scripts

---
+++ packages/dump1090/scripts/dump.sh
@@ -0,0 +1,1 @@
+./../dump1090-fa/dump1090 --net --interactive --forward-mlat --write-json ../dumpfiles/ --lat 38.860934 --lon -77.048562 --gain 26
---
+++ packages/dump1090/scripts/gain.sh
@@ -0,0 +1,1 @@
+awk "$(cat ./dumpfiles/stats.json| grep total | sed 's/.*accepted":\[\([0-9]*\).*strong_signals":\([0-9]*\).*/BEGIN {printf "\\nPercentage of strong messages: %.3f \\n" , \2 * 100 \/ \1}/')"

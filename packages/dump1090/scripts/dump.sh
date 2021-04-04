e73564731c5f3cd5113d85dd0033363b0bbe0358
Robert Steilberg <rsteilberg@gmail.com>
2021-04-03 23:02:36 -0400

    adjust gain

--- packages/dump1090/scripts/dump.sh
+++ packages/dump1090/scripts/dump.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./../dump1090-fa/dump1090 --net --interactive --forward-mlat --json-location-accuracy 2 --write-json ../dumpfiles/ --lat 38.860934 --lon -77.048562 --gain 20
+./../dump1090-fa/dump1090 --net --interactive --forward-mlat --json-location-accuracy 2 --write-json ../dumpfiles/ --lat 38.860934 --lon -77.048562 --gain 21

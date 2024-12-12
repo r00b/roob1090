8507b165d0e95588b4807e3da42ab4545b1da773
Robert Steilberg <rsteilberg@gmail.com>
2024-12-12 10:48:56 -0600

    modify dump1090 scripts

---
+++ packages/dump1090/scripts/dump-debug.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dump1090-fa --net --interactive --forward-mlat --write-json ../dumpfiles/ --json-location-accuracy 2 --lat 30.314219 --lon -97.736262 --gain 36
--- packages/dump1090/scripts/dump.sh
+++ packages/dump1090/scripts/dump.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dump1090-fa --net --interactive --forward-mlat --write-json ../dumpfiles/ --json-location-accuracy 2 --lat 30.314219 --lon -97.736262 --gain 36
+dump1090-fa --net --forward-mlat --write-json ../dumpfiles/ --json-location-accuracy 2 --lat 30.314219 --lon -97.736262 --gain 36
